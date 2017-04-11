ceef9497355e2bd9fa57042120c361960c46c1a8
super-ai <294473456@qq.com>
2017-04-11 15:41:12 +0800

    起始版本

---
+++ index.js
@@ -0,0 +1,11 @@
+// document.body.innerHTML = '<div>cmtt!</div>';
+// console.info('cmtt!');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './modules/App'
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('example')
+)
+
